d18842f7709f2ad437b946149405fb95a0473a1d
Tharanga Kasthuriarachchi <tkasthuriarachchi@pccwglobal.com>
2021-12-07 15:41:54 +1000

    Fix linting

--- Day1/part1.ts
+++ Day1/part1.ts
@@ -1,22 +1,22 @@
 import { readFile } from "./readFile";
 
 export const findLargerMesurements = (numArr) => {
-    const firstNum = numArr[0];
-    let largerThan = 0;
+  const firstNum = numArr[0];
+  let largerThan = 0;
 
-    if (numArr[1] > firstNum) {
+  if (numArr[1] > firstNum) {
+      largerThan = largerThan + 1;
+  }
+
+  for (let i:number = 2; i < numArr.length; i++) {
+    if(i >= numArr.length) {
+      break;
+    } else {
+      if (numArr[i] > numArr[i - 1]) {
         largerThan = largerThan + 1;
+      }
     }
-
-    for (let i:number = 2; i < numArr.length; i++) {
-        if(i >= numArr.length) {
-            break;
-        } else {
-            if (numArr[i] > numArr[i - 1]) {
-                largerThan = largerThan + 1;
-            }
-        }
-    }
+  }
     return largerThan;
 }
 
