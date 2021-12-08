70b9e590cc6c63c4297f4f23407ae360094bed6a
Tharanga Kasthuriarachchi <tkasthuriarachchi@pccwglobal.com>
2021-12-08 17:50:39 +1000

    Get day 3A logic right

--- Day3/part1.ts
+++ Day3/part1.ts
@@ -1,75 +1,49 @@
+const report: string[] = [
+  "00100",
+  "11110",
+  "10110",
+  "10111",
+  "10101",
+  "01111",
+  "00111",
+  "11100",
+  "10000",
+  "11001",
+  "00010",
+  "01010"
+];
+
 export const task1Example = (diagnosticReport: string[]) => {
 
-  let firstDigitOnes = 0;
-  let firstDigitZeroes = 0;
+  let diagnosticReportRecordLength = diagnosticReport[0].length;
 
-  let secondDigitOnes = 0;
-  let secondDigitZeroes = 0;
-
-  let thirdDigitOnes = 0;
-  let thirdDigitZeroes = 0;
-
-  let fourthDigitOnes = 0;
-  let fourthDigitZeroes = 0;
-
-  let fifthDigitOnes = 0;
-  let fifthDigitZeroes = 0;
+  let digitOnes: number[] = new Array(diagnosticReportRecordLength).fill(0);
+  let digitZeroes: number[] = new Array(diagnosticReportRecordLength).fill(0);
+ 
 
   // TODO: rewrite this to be for any length of number
   for (let i: number = 0; i < diagnosticReport.length; i++) {
-
-    let firstDigit = diagnosticReport[i][0];
-    (firstDigit === '1') ? firstDigitOnes++ : firstDigitZeroes++;
-  
-    let secondDigit = diagnosticReport[i][1];
-    (secondDigit === '1') ? secondDigitOnes++ : secondDigitZeroes++;
-
-    let thirdDigit = diagnosticReport[i][2];
-    (thirdDigit === '1') ? thirdDigitOnes++ : thirdDigitZeroes++;
-
-    let fourthDigit = diagnosticReport[i][3];
-    (fourthDigit === '1') ? fourthDigitOnes++ : fourthDigitZeroes++;
-
-    let fifthDigit = diagnosticReport[i][4];
-    (fifthDigit === '1') ? fifthDigitOnes++ : fifthDigitZeroes++;
+    for (let j: number = 0; j < diagnosticReportRecordLength; j++) {
+      (diagnosticReport[i][j] === '1') ? digitOnes[j]++ : digitZeroes[j]++;
+    }
   }
 
   let gammaRateString: string = '';
+  let epsilonRateString: string = '';
 
-  const gammaFirstDigit = (firstDigitOnes > firstDigitZeroes) ? '1' : '0';
-  const gammaSecondDigit = (secondDigitOnes > secondDigitZeroes) ? '1' : '0';
-  const gammaThirdDigit = (thirdDigitOnes > thirdDigitZeroes) ? '1' : '0';
-  const gammaFourthDigit = (fourthDigitOnes > fourthDigitZeroes) ? '1' : '0';
-  const gammaFifthDigit = (fifthDigitOnes > fifthDigitZeroes) ? '1' : '0';
+  for (let x: number = 0; x < diagnosticReportRecordLength; x++) {
+    const gammaDigit = (digitOnes[x] > digitZeroes[x]) ? '1' : '0';
+    const epsilonDigit = (gammaDigit === '1') ? '0' : '1';
 
-  gammaRateString = gammaFirstDigit 
-    + gammaSecondDigit 
-    + gammaThirdDigit 
-    + gammaFourthDigit
-    + gammaFifthDigit;
+    gammaRateString += gammaDigit;
+    epsilonRateString += epsilonDigit;
+  }
 
-  const epsilonFirstDigit = (gammaFirstDigit === '1') ? '0' : '1';
-  const epsilonSecondDigit = (gammaSecondDigit === '1') ? '0' : '1';
-  const epsilonThirdDigit = (gammaThirdDigit === '1') ? '0' : '1';
-  const epsilonFourthDigit = (gammaFourthDigit === '1') ? '0' : '1';
-  const epsilonFifthDigit = (gammaFifthDigit === '1') ? '0' : '1';
-
-  const epsilonRateString = epsilonFirstDigit 
-    + epsilonSecondDigit 
-    + epsilonThirdDigit 
-    + epsilonFourthDigit 
-    + epsilonFifthDigit;
-
-  const gammaRate = convertBinaryToDecimal(gammaRateString);
-  const epsilonRate = convertBinaryToDecimal(epsilonRateString);
+  const gammaRate = parseInt(gammaRateString, 2);
+  const epsilonRate = parseInt(epsilonRateString, 2);
 
   return gammaRate * epsilonRate;
 }
 
-const convertBinaryToDecimal = (binaryString: string) => {
-  return parseInt((binaryString + '')
-  .replace(/[^01]/gi, ''), 2);
-}
 
-
-// task1Example(report);
+task1Example(report);
