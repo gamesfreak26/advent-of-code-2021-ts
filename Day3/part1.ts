fbced99cc7709fd08a96052db35eeddbec9ea6f2
Tharanga Kasthuriarachchi <tkasthuriarachchi@pccwglobal.com>
2021-12-10 14:20:23 +1000

    Refactor day3 part 1

--- Day3/part1.ts
+++ Day3/part1.ts
@@ -15,25 +15,22 @@ const report: string[] = [
 
 export const task1 = (diagnosticReport: string[]) => {
 
-  let diagnosticReportRecordLength = diagnosticReport[0].length;
-
-  let digitOnes: number[] = new Array(diagnosticReportRecordLength).fill(0);
-  let digitZeroes: number[] = new Array(diagnosticReportRecordLength).fill(0);
- 
-
-  // TODO: rewrite this to be for any length of number
-  for (let i: number = 0; i < diagnosticReport.length; i++) {
-    for (let j: number = 0; j < diagnosticReportRecordLength; j++) {
-      (diagnosticReport[i][j] === '1') ? digitOnes[j]++ : digitZeroes[j]++;
-    }
-  }
-
   let gammaRateString: string = '';
   let epsilonRateString: string = '';
 
-  for (let x: number = 0; x < diagnosticReportRecordLength; x++) {
-    const gammaDigit = (digitOnes[x] > digitZeroes[x]) ? '1' : '0';
-    const epsilonDigit = (gammaDigit === '1') ? '0' : '1';
+  let diagnosticReportRecordLength = diagnosticReport[0].length;
+
+  for (let j: number = 0; j < diagnosticReportRecordLength; j++) {
+
+    let digitOnes: number = 0;
+    let digitZeroes: number = 0;
+
+    diagnosticReport.forEach(line => {
+      (line[j] === '1') ? digitOnes++ : digitZeroes++;
+    })
+
+    let gammaDigit = (digitOnes > digitZeroes) ? '1' : '0';
+    let epsilonDigit = (gammaDigit === '1') ? '0' : '1';
 
     gammaRateString += gammaDigit;
     epsilonRateString += epsilonDigit;
@@ -44,6 +41,5 @@ export const task1 = (diagnosticReport: string[]) => {
 
   return gammaRate * epsilonRate;
 }
-
 
 task1(report);
