5a4ac16888541eb3a2ec9ac0119e6e3eac215c1e
Tharanga Kasthuriarachchi <tkasthuriarachchi@pccwglobal.com>
2021-12-13 15:41:55 +1000

    Start Day 4

---
+++ Day4/part1.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import { readFile } from "../Day1/readFile";
+
+const file = readFile(
+    path.join(
+      __dirname, 
+      '../../adventOfCode/Day4/inputs/testInput.txt'
+    )
+  );
+
+const a = 5;
