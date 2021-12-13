814b3cb47fd3d748a3a80487a8a4dadb015e49ea
Tharanga Kasthuriarachchi <tkasthuriarachchi@pccwglobal.com>
2021-12-13 17:12:54 +1000

    Filter input to return boards as a array and return the random numbers

--- Day4/part1.ts
+++ Day4/part1.ts
@@ -8,4 +8,41 @@ const file = readFile(
     )
   );
 
-const a = 5;
+const task1 = (input: string[]) => {
+  const splitFile = file.split('\n\n');
+
+  const randomNumbers = splitFile[0];
+  
+  let boardsStrings = [];
+ 
+  
+  // Each board is an array of length 5 but is the string of numbers.
+  for (let i = 1; i < splitFile.length; i++) {
+      boardsStrings.push(splitFile[i].split('\n'));
+  }
+
+  const filteredBoards = filterBoards(boardsStrings);
+  let a = 5;
+}
+
+const filterBoards = (boardArr: string[]) => {
+  
+  let boards = [];
+  
+  for (let j = 0; j < boardArr.length; j++) {
+    for (let k = 0; k < 5; k ++) {
+      boards.push(boardArr[j][k].split(' '))
+    }  
+  }
+  
+  let filteredBoards = [];
+
+  for (let x = 0; x < boards.length; x++) {
+    filteredBoards.push(boards[x].filter((val: string) => val));
+  }
+  
+  return filteredBoards;
+}
+
+task1(file);
+let a = 5;
